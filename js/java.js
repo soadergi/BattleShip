36c9f2d28689c2ca16110f134e1f4aa354b8743f
soadergi <soadergi@gmail.com>
2016-10-25 19:55:35 +0300

    add td tabledata id

---
+++ js/java.js
@@ -0,0 +1,21 @@
+window.onload = view.init;
+
+var view = {
+	displayMessage: function(){},
+	displayHit: function(){},
+	displayMiss: function(){},
+	init: function(){
+		model.generateShipLocation();
+	},
+	handleFire: function(){},
+}
+
+var controller = {
+
+}
+
+var model = {
+
+}
+
+mvc pattern
