c859bbecda7ae744c9094cf0d144db34acba0965
Anatoliy <soadergi@gmail.com>
2016-10-27 14:49:23 +0300

    Add user input and validation

--- js/java.js
+++ js/java.js
@@ -1,24 +1,35 @@
-window.onload = view.init;
+//window.onload = view.init;
 
 var view = {
-	displayMessage: function(){},
-	displayHit: function(){},
-	displayMiss: function(){},
+	//displayMessage: function(){},
+	//displayHit: function(){},
+	//displayMiss: function(){},
 	init: function(){
-		model.generateShipLocation();
+		//model.generateShipLocation();
+		document.getElementById("button").addEventListener("click", controller.getUserInput);
 	},
-	handleFire: function(){},
+	//handleFire: function(){},
 }
 
 var controller = {
-
+	getUserInput: function(){
+		var user_input = document.getElementById("sector_input").value;
+		console.log(user_input);
+		var ABCDEFG = ["A","B","C","D","E","F","G"];
+		var input = ABCDEFG.indexOf(user_input[0]) + user_input[1];
+		console.log(input);
+		return input
+	}
 }
 
 var model = {
-
+	//generateShipLocation: function(){},
 }
 
-mvc pattern
-document.getElementById()
-indexOf
-charAt
+
+window.onload = view.init;
+
+//mvc pattern
+//document.getElementById()
+//indexOf
+//charAt
